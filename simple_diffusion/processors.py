5069ecefe20822dee6cf3562991a4d5f8af96b00
pdoane <pdoane2@gmail.com>
2023-04-13 08:59:01 -0700

    Use controlnet_aux 0.0.2

--- simple_diffusion/processors.py
+++ simple_diffusion/processors.py
@@ -7,53 +7,68 @@ import cv2
 import numpy as np
 import torch
 import utils
-from controlnet_aux import HEDdetector, MLSDdetector, OpenposeDetector
+from controlnet_aux import (CannyDetector, HEDdetector, MidasDetector,
+                            MLSDdetector, OpenposeDetector)
+from controlnet_aux import util as controlnet_utils
+from controlnet_aux.midas.api import MiDaSInference
+from einops import rearrange
+from huggingface_hub import hf_hub_download
 from PIL import Image
-from transformers import (AutoImageProcessor, UperNetForSemanticSegmentation,
-                          pipeline)
+from transformers import AutoImageProcessor, UperNetForSemanticSegmentation
 
 
-# From controlnet_utils
-def ade_palette():
-    """ADE20K palette that maps each class to RGB values."""
-    return [[120, 120, 120], [180, 120, 120], [6, 230, 230], [80, 50, 50],
-            [4, 200, 3], [120, 120, 80], [140, 140, 140], [204, 5, 255],
-            [230, 230, 230], [4, 250, 7], [224, 5, 255], [235, 255, 7],
-            [150, 5, 61], [120, 120, 70], [8, 255, 51], [255, 6, 82],
-            [143, 255, 140], [204, 255, 4], [255, 51, 7], [204, 70, 3],
-            [0, 102, 200], [61, 230, 250], [255, 6, 51], [11, 102, 255],
-            [255, 7, 71], [255, 9, 224], [9, 7, 230], [220, 220, 220],
-            [255, 9, 92], [112, 9, 255], [8, 255, 214], [7, 255, 224],
-            [255, 184, 6], [10, 255, 71], [255, 41, 10], [7, 255, 255],
-            [224, 255, 8], [102, 8, 255], [255, 61, 6], [255, 194, 7],
-            [255, 122, 8], [0, 255, 20], [255, 8, 41], [255, 5, 153],
-            [6, 51, 255], [235, 12, 255], [160, 150, 20], [0, 163, 255],
-            [140, 140, 140], [250, 10, 15], [20, 255, 0], [31, 255, 0],
-            [255, 31, 0], [255, 224, 0], [153, 255, 0], [0, 0, 255],
-            [255, 71, 0], [0, 235, 255], [0, 173, 255], [31, 0, 255],
-            [11, 200, 200], [255, 82, 0], [0, 255, 245], [0, 61, 255],
-            [0, 255, 112], [0, 255, 133], [255, 0, 0], [255, 163, 0],
-            [255, 102, 0], [194, 255, 0], [0, 143, 255], [51, 255, 0],
-            [0, 82, 255], [0, 255, 41], [0, 255, 173], [10, 0, 255],
-            [173, 255, 0], [0, 255, 153], [255, 92, 0], [255, 0, 255],
-            [255, 0, 245], [255, 0, 102], [255, 173, 0], [255, 0, 20],
-            [255, 184, 184], [0, 31, 255], [0, 255, 61], [0, 71, 255],
-            [255, 0, 204], [0, 255, 194], [0, 255, 82], [0, 10, 255],
-            [0, 112, 255], [51, 0, 255], [0, 194, 255], [0, 122, 255],
-            [0, 255, 163], [255, 153, 0], [0, 255, 10], [255, 112, 0],
-            [143, 255, 0], [82, 0, 255], [163, 255, 0], [255, 235, 0],
-            [8, 184, 170], [133, 0, 255], [0, 255, 92], [184, 0, 255],
-            [255, 0, 31], [0, 184, 255], [0, 214, 255], [255, 0, 112],
-            [92, 255, 0], [0, 224, 255], [112, 224, 255], [70, 184, 160],
-            [163, 0, 255], [153, 0, 255], [71, 255, 0], [255, 0, 163],
-            [255, 204, 0], [255, 0, 143], [0, 255, 235], [133, 255, 0],
-            [255, 0, 235], [245, 0, 255], [255, 0, 122], [255, 245, 0],
-            [10, 190, 212], [214, 255, 0], [0, 204, 255], [20, 0, 255],
-            [255, 255, 0], [0, 153, 255], [0, 41, 255], [0, 255, 204],
-            [41, 0, 255], [41, 255, 0], [173, 0, 255], [0, 245, 255],
-            [71, 0, 255], [122, 0, 255], [0, 255, 184], [0, 92, 255],
-            [184, 255, 0], [0, 133, 255], [255, 214, 0], [25, 194, 194],
-            [102, 255, 0], [92, 0, 255]]
+# Fix issues in controlnet_aux 0.0.2 requiring Cuda
+class MidasDetector:
+    def __init__(self, model_type="dpt_hybrid", model_path=None):
+        self.model = MiDaSInference(model_type=model_type, model_path=model_path)
+        if torch.cuda.is_available():
+            self.model = self.model.cuda()
+
+    @classmethod
+    def from_pretrained(cls, pretrained_model_or_path, model_type="dpt_hybrid", filename=None, cache_dir=None):
+        filename = filename or "annotator/ckpts/dpt_hybrid-midas-501f0c75.pt"
+        model_path = hf_hub_download(pretrained_model_or_path, filename, cache_dir=cache_dir)
+        return cls(model_type=model_type, model_path=model_path)
+        
+    def __call__(self, input_image, a=np.pi * 2.0, bg_th=0.1):
+        
+        input_type = "np"
+        if isinstance(input_image, Image.Image):
+            input_image = np.array(input_image)
+            input_type = "pil"
+            
+        input_image = controlnet_utils.HWC3(input_image)
+        image_depth = input_image
+        with torch.no_grad():
+            image_depth = torch.from_numpy(image_depth).float()
+            if torch.cuda.is_available():
+                image_depth = image_depth.cuda()
+            image_depth = image_depth / 127.5 - 1.0
+            image_depth = rearrange(image_depth, 'h w c -> 1 c h w')
+            depth = self.model(image_depth)[0]
+
+            depth_pt = depth.clone()
+            depth_pt -= torch.min(depth_pt)
+            depth_pt /= torch.max(depth_pt)
+            depth_pt = depth_pt.cpu().numpy()
+            depth_image = (depth_pt * 255.0).clip(0, 255).astype(np.uint8)
+
+            depth_np = depth.cpu().numpy()
+            x = cv2.Sobel(depth_np, cv2.CV_32F, 1, 0, ksize=3)
+            y = cv2.Sobel(depth_np, cv2.CV_32F, 0, 1, ksize=3)
+            z = np.ones_like(x) * a
+            x[depth_pt < bg_th] = 0
+            y[depth_pt < bg_th] = 0
+            normal = np.stack([x, y, z], axis=2)
+            normal /= np.sum(normal ** 2.0, axis=2, keepdims=True) ** 0.5
+            normal_image = (normal * 127.5 + 127.5).clip(0, 255).astype(np.uint8)
+        
+        if input_type == "pil":
+            depth_image = Image.fromarray(depth_image)
+            depth_image = depth_image.convert("RGB")
+            normal_image = Image.fromarray(normal_image)
+        
+        return depth_image, normal_image
 
 class ProcessorBase(ABC):
     @abstractmethod
@@ -61,68 +76,39 @@ class ProcessorBase(ABC):
         pass
 
 class CannyProcessor(ProcessorBase):
+    low_threshold = 100
+    high_threshold = 100
+
     def __init__(self) -> None:
         super().__init__()
+        self.canny = None
 
     def __call__(self, image: Image.Image) -> Image.Image:
-        low_threshold = 100
-        high_threshold = 100
-        image = np.array(image)
-        image = cv2.Canny(image, low_threshold, high_threshold)
-        image = image[:, :, None]
-        image = np.concatenate([image, image, image], axis=2)
-        image = Image.fromarray(image)
+        if self.canny is None:
+            self.canny = CannyDetector()
+        image = self.canny(image, self.low_threshold, self.high_threshold)
         return image
 
 class DepthProcessor(ProcessorBase):
     def __init__(self) -> None:
         super().__init__()
-        self.depth_estimator = None
+        self.midas = None
 
     def __call__(self, image: Image.Image) -> Image.Image:
-        if self.depth_estimator is None:
-            self.depth_estimator = pipeline('depth-estimation', 'Intel/dpt-large')
-        image = self.depth_estimator(image)['depth']
-        image = np.array(image)
-        image = image[:, :, None]
-        image = np.concatenate([image, image, image], axis=2)
-        image = Image.fromarray(image)
+        if self.midas is None:
+            self.midas = MidasDetector.from_pretrained('lllyasviel/ControlNet')
+        image, _ = self.midas(image)
         return image
 
 class NormalProcessor(ProcessorBase):
     def __init__(self) -> None:
         super().__init__()
-        self.depth_estimator = None
+        self.midas = None
 
     def __call__(self, image: Image.Image) -> Image.Image:
-        if self.depth_estimator is None:
-            self.depth_estimator = pipeline('depth-estimation', 'Intel/dpt-hybrid-midas')
-
-        # predicted-depth image is smaller so resize at the end
-        original_size = (image.width, image.height)
-
-        image = self.depth_estimator(image)['predicted_depth'][0]
-        image = image.numpy()
-
-        image_depth = image.copy()
-        image_depth -= np.min(image_depth)
-        image_depth /= np.max(image_depth)
-
-        bg_threhold = 0.4
-
-        x = cv2.Sobel(image, cv2.CV_32F, 1, 0, ksize=3)
-        x[image_depth < bg_threhold] = 0
-
-        y = cv2.Sobel(image, cv2.CV_32F, 0, 1, ksize=3)
-        y[image_depth < bg_threhold] = 0
-
-        z = np.ones_like(x) * np.pi * 2.0
-
-        image = np.stack([x, y, z], axis=2)
-        image /= np.sum(image ** 2.0, axis=2, keepdims=True) ** 0.5
-        image = (image * 127.5 + 127.5).clip(0, 255).astype(np.uint8)
-        image = cv2.resize(image, original_size)
-        image = Image.fromarray(image)
+        if self.midas is None:
+            self.midas = MidasDetector.from_pretrained('lllyasviel/ControlNet')
+        _, image = self.midas(image)
         return image
 
 class HedProcessor(ProcessorBase):
@@ -185,7 +171,7 @@ class SegProcessor(ProcessorBase):
             outputs = self.image_segmentor(pixel_values)
         seg = self.image_processor.post_process_semantic_segmentation(outputs, target_sizes=[image.size[::-1]])[0]
         color_seg = np.zeros((seg.shape[0], seg.shape[1], 3), dtype=np.uint8)
-        palette = np.array(ade_palette())
+        palette = np.array(controlnet_utils.ade_palette())
         for label, color in enumerate(palette):
             color_seg[seg == label, :] = color
         color_seg = color_seg.astype(np.uint8)
@@ -212,9 +198,8 @@ class ESRGANProcessor(ProcessorBase):
         with warnings.catch_warnings(), utils.ChangeDirectory(configuration.MODELS_PATH):
             warnings.simplefilter('ignore')
             from basicsr.archs.rrdbnet_arch import RRDBNet
-            from realesrgan.archs.srvgg_arch import SRVGGNetCompact
-
             from realesrgan import RealESRGANer
+            from realesrgan.archs.srvgg_arch import SRVGGNetCompact
 
             if self.model_name == 'RealESRGAN_x4plus':  # x4 RRDBNet model
                 model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
